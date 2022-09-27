d860a7d7b64cdb55a92b3db3b33be216ed430dda
matpl11 <84911097+matpl11@users.noreply.github.com>
2022-09-27 11:54:01 +0200

    First commit

---
+++ src/index.ts
@@ -0,0 +1,64 @@
+// Generator numeracji blankietów ścisłego zarachowania
+// (C) 2022 matpl11. Kod dostępny na licencji GNU GPL v3
+
+
+import Express from "express"
+import pdf from 'html-pdf' // na przyszłość/for future use
+import bodyParser from "body-parser" // na przyszłość/for future use
+import dotenv from 'dotenv'
+
+import { iterateTemplate } from "./iterator"
+
+dotenv.config({
+    path: 'config.env'
+})
+const App: Express.Application = Express()
+const PORT = process.env.HTTP_PORT
+let tickets = {
+    start: 0,
+    digits: 0,
+    serie: 'AA',
+    qty: 1
+}
+
+if(typeof process.env.TICKET_STARTING === 'string'){
+    tickets.start = parseInt(process.env.TICKET_STARTING)
+}
+else tickets.digits = 0
+
+if(typeof process.env.TICKET_QTY === 'string'){
+    tickets.qty = parseInt(process.env.TICKET_QTY)
+}
+else tickets.qty = 0
+
+if(typeof process.env.SERIE === 'string'){
+    tickets.serie = process.env.SERIE
+}
+else tickets.serie = ''
+
+if(typeof process.env.DIGITS === 'string'){
+    tickets.digits = parseInt(process.env.DIGITS)
+}
+else tickets.digits = 0
+
+/*
+
+Objaśnienie funkcji strzałkowej
+
+const async iterateTemplate()
+
+start - pierwszy numer, od którego chcemy iterować blankiety
+quantity - ilość blankietów do generowania
+digits - ilość cyfr, które muszą znaleźć się w numerze blankietu
+serie - jego seria
+
+*/
+
+App.get('/', function (req, res) {
+    iterateTemplate(tickets.start, tickets.qty, tickets.digits, 'AQ').then(function (r) {
+        res.send(r)
+    })
+})
+
+App.listen(PORT)
+console.log(`listening at ${PORT}`)
---
+++ src/iterator.ts
@@ -0,0 +1,19 @@
+// Generator numeracji blankietów ścisłego zarachowania
+// (C) 2022 matpl11. Kod dostępny na licencji GNU GPL v3
+
+import ejs from 'ejs'
+
+const iterateTemplate = async (start: number, quantity: number, digits: number, serie: string) => {
+    let resp: string = '', realNum: string = ''
+    for (var i = start; i < (start + quantity); i++) {
+        realNum = i.toString()
+        for (var j = digits - realNum.length; j > 0; j--) {
+            realNum = '0' + realNum
+        }
+        resp += await ejs.renderFile('views/blankiet.ejs', { serie: serie, num: realNum })
+    }
+
+    return resp
+}
+
+export { iterateTemplate }
