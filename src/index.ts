a998a4ffa92ae9b64de041b0c1dca90d9369552c
matpl11 <84911097+matpl11@users.noreply.github.com>
2022-09-28 18:08:38 +0200

    Podstawowa walidacja

--- src/index.ts
+++ src/index.ts
@@ -22,22 +22,22 @@ let tickets = {
     qty: 1
 }
 
-if(typeof process.env.TICKET_STARTING === 'string'){
+if (typeof process.env.TICKET_STARTING === 'string') {
     tickets.start = parseInt(process.env.TICKET_STARTING)
 }
 else tickets.digits = 0
 
-if(typeof process.env.TICKET_QTY === 'string'){
+if (typeof process.env.TICKET_QTY === 'string') {
     tickets.qty = parseInt(process.env.TICKET_QTY)
 }
 else tickets.qty = 0
 
-if(typeof process.env.SERIE === 'string'){
+if (typeof process.env.SERIE === 'string') {
     tickets.serie = process.env.SERIE
 }
 else tickets.serie = ''
 
-if(typeof process.env.DIGITS === 'string'){
+if (typeof process.env.DIGITS === 'string') {
     tickets.digits = parseInt(process.env.DIGITS)
 }
 else tickets.digits = 0
@@ -61,15 +61,36 @@ serie - jego seria
 */
 
 App.get('/', function (req, res) {
-    ejs.renderFile('views/main.ejs').then((r)=>{
+    ejs.renderFile('views/main.ejs').then((r) => {
         res.send(r)
     })
 })
 
 App.post('/getResult', function (req, res) {
-    iterateTemplate(parseInt(req.body.start), parseInt(req.body.count), parseInt(req.body.dig), req.body.serie).then(function (r) {
-        res.send(r)
-    })
+
+    const start = parseInt(req.body.start)
+    const ticketQty = parseInt(req.body.count)
+    const digitsQty = parseInt(req.body.dig)
+    const serie = req.body.serie
+
+    if (
+        (Number.isNaN(start) == false) &&
+        (Number.isNaN(ticketQty) == false) &&
+        (Number.isNaN(digitsQty) == false) &&
+        (serie != '' && serie != null)
+    ) {
+        iterateTemplate(start, ticketQty, digitsQty, serie).then(function (r) {
+            res.send(r)
+        })
+    }
+    else {
+        ejs.renderFile('views/main.ejs',
+            {
+                alert: 'Wprowadzone dane są błędne.'
+            }).then((r) => {
+                res.send(r)
+            })
+    }
 })
 
 App.listen(PORT)
